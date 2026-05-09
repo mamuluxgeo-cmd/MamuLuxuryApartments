741c5f9b4b4345c1df75d445647ad9a8c2a2a6ce
mamuluxgeo-cmd <mamuluxgeo@gmail.com>
2026-05-09 15:52:29 +0400

    Add Google Apps Script Web App URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GOOGLE_SCRIPT_URL: '',
+  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzqiqYdxtB2Rng7LncSpKMyESEPCWfNBtl-4-QNFQAWGNkMDpwwJekPa_z728H6LL4K/exec',
   IMGBB_API_KEY: '104cf2ebb4a05c7908280dff041c8609',
   DEFAULT_THEME: 'night'
 };
