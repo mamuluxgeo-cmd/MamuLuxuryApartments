ea4b3c039617e00e91de5ba2794047e8032d3fb0
mamuluxgeo-cmd <mamuluxgeo@gmail.com>
2026-06-26 13:20:15 +0400

    Add OtelMS booking URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
   GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzqiqYdxtB2Rng7LncSpKMyESEPCWfNBtl-4-QNFQAWGNkMDpwwJekPa_z728H6LL4K/exec',
   IMGBB_API_KEY: '104cf2ebb4a05c7908280dff041c8609',
-  DEFAULT_THEME: 'night'
+  DEFAULT_THEME: 'night',
+  OTELMS_BOOKING_URL: 'https://booking-116144.otelms.com/booking/rooms'
 };
